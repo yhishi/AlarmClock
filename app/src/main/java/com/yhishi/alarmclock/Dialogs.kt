58fe2d32bb759ecab0e89fbfcbde3739779703f4
yhishi <hishikawakeiki@hishi-Mac.local>
2018-06-26 01:25:24 +0900

    日付と時刻を選択するためのダイアログ追加

--- app/src/main/java/com/yhishi/alarmclock/Dialogs.kt
+++ app/src/main/java/com/yhishi/alarmclock/Dialogs.kt
@@ -1,10 +1,16 @@
 package com.yhishi.alarmclock
 
+import android.app.DatePickerDialog
 import android.app.Dialog
+import android.app.TimePickerDialog
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.support.v7.app.AlertDialog
+import android.widget.DatePicker
+import android.widget.TimePicker
+import java.util.*
+import kotlin.math.min
 
 // ダイアログ用クラス
 class SimapleAlertDialog: DialogFragment() {
@@ -43,6 +49,70 @@ class SimapleAlertDialog: DialogFragment() {
             }
         }
         return builder.create()
+    }
+}
+
+// 日付を選択するダイアログ
+class DatePickerFragment: DialogFragment(),
+        DatePickerDialog.OnDateSetListener {
+
+    interface OnDateSelectedListener {
+        // 日付が選択された時の処理
+        fun onSelected(year: Int, month: Int, date: Int)
+    }
+
+    private lateinit var listener: OnDateSelectedListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if(context is OnDateSelectedListener) {
+            listener = context
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val date = c.get(Calendar.DAY_OF_MONTH)
+        // DatePickerDialogのコンストラクタでインスタンスを生成し、返却（現在時刻を初期値設定）
+        return DatePickerDialog(context, this, year, month, date)
+    }
+
+    // 日付が選択された時の処理
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, date: Int) {
+        listener.onSelected(year, month, date)
+    }
+}
+
+// 時刻を選択するダイアログ
+class TimePickerFragment: DialogFragment(),
+        TimePickerDialog.OnTimeSetListener {
+
+    interface OnTimeSelectedListener {
+        fun onSelected(hourOfDay: Int, minute: Int)
+    }
+
+    private lateinit var listener: OnTimeSelectedListener
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if(context is TimePickerFragment.OnTimeSelectedListener) {
+            listener = context
+        }
+    }
+
+    // 現在時刻の設定
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val c = Calendar.getInstance()
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+        return TimePickerDialog(context, this, hour, minute, true)
+    }
+
+    // 時刻選択時の処理
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        listener.onSelected(hourOfDay, minute)
     }
 
 }
